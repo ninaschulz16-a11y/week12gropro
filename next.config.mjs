e2019877d2154be4c8d547c3b6cd70c0b6f71869
grazielac <cpgraziela@gmail.com>
2025-11-19 11:03:29 +0000

    Add fake data + domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['iykwgozjyetlwoftbjgx.supabase.co'], // Add your Supabase storage host here
+    domains: ['iykwgozjyetlwoftbjgx.supabase.co'],
+    domains: ["i.pravatar.cc", "iykwgozjyetlwoftbjgx.supabase.co"], // Add your Supabase storage host here
   },
   /* config options here */
 };
