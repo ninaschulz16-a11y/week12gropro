29828dd79291b90c62309cde363f58314e8ed2b6
Hetal Shah <ducksareyellowish@gmail.com>
2025-11-19 15:53:32 +0000

    fixed functionality

--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,9 +1,23 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-green-50">
-        <SignUp />
+        <div className="min-h-screen flex items-center justify-center bg-[#F5F5DC]">
+        <SignUp 
+            routing="path"
+            path="/sign-up"
+            afterSignUpUrl="/home"
+            appearance={{
+            elements: {
+                formButtonPrimary: {
+                backgroundColor: "#3E513E",
+                "&:hover": {
+                    backgroundColor: "#2d3d2d"
+                }
+                }
+            }
+            }}
+        />
         </div>
-    )
+    );
 }
